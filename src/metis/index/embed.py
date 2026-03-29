b179a6efe062be69c69331cb02448e804b462ea7
itsautomata <112640947+itsautomata@users.noreply.github.com>
2026-03-29 22:46:28 +0200

    mend embed: batch API calls to prevent overflow on large documents

--- src/metis/index/embed.py
+++ src/metis/index/embed.py
@@ -3,14 +3,21 @@
 from metis.client import get_client, get_embedding_model
 from metis.config import MetisConfig
 
+BATCH_SIZE = 40
+
 
 def embed_texts(texts: list[str], config: MetisConfig) -> list[list[float]]:
-    """embed a list of texts. returns list of vectors."""
+    """embed a list of texts in batches. returns list of vectors."""
+    if not texts:
+        return []
+
     client = get_client(config)
+    model = get_embedding_model(config)
 
-    response = client.embeddings.create(
-        model=get_embedding_model(config),
-        input=texts,
-    )
+    all_embeddings = []
+    for i in range(0, len(texts), BATCH_SIZE):
+        batch = texts[i : i + BATCH_SIZE]
+        response = client.embeddings.create(model=model, input=batch)
+        all_embeddings.extend(item.embedding for item in response.data)
 
-    return [item.embedding for item in response.data]
+    return all_embeddings
